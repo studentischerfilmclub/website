d66d9ac575cefa9645937a3325b0e65fa7f3f29d
Max-Hi <levin.hirsch@web.de>
2023-09-06 17:18:05 +0200

    fixed minor bug regarding event display

--- api/routers/events.py
+++ api/routers/events.py
@@ -17,13 +17,16 @@ router = APIRouter(
 
 @router.post("/post")
 async def write_event(event_data: EventData, user: Annotated[User, Depends(is_member)]):
+    logging.info(f"0 {event_data}")
     event = jsonable_encoder(event_data)
+    logging.info(f"1 {event}")
     event["datetime"] = datetime.datetime.fromisoformat(event["date"] + "T" + event["time"])
     del event["date"]
     del event["time"]
     if not event["link"]:
         del event["link"]
-
+    logging.info(f"2 {event}")
+    
     # check if document is inserted
     if db.events.count_documents(event) != 0:
         logging.info(f"Event already present: {event}")
@@ -50,7 +53,7 @@ async def get_all_events() -> list[dict]:
 @router.get("/next", response_model=list[Event])
 async def get_next_events() -> list[dict]:
     return list(
-        db.events.find({"datetime": {"$gte": datetime.datetime.now()}})
-        .sort("datetime", pymongo.ASCENDING)
+        db.events.find({})#"datetime": {"$gte": datetime.datetime.now()}})
+        #.sort("datetime", pymongo.ASCENDING)
     )
 
