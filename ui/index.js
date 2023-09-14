3f1fe8d08ffa94cd97754a68384853ec78bca306
Joseph Holten <joseph@holten.com>
2023-09-14 17:09:51 +0200

    fixed wlan issue

--- ui/index.js
+++ ui/index.js
@@ -5,16 +5,31 @@ import {getNextEvents} from "./events.js"
 window.addEventListener("load", onLoad)
 let socket;
 
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
 async function onLoad() {
     getNextEvents()
     getElections()
 
+    let websocket_id = localStorage.getItem("websocket_id")
+    if (websocket_id === null) {
+        websocket_id = makeid(16)
+        localStorage.setItem("websocket_id", websocket_id)
+    } 
     // connect to websocket
-    fetchApi("GET", "elections/websocket_id")
-    .then((id) => {
-        socket = websocketApi("elections/websocket", id)
-        socket.onmessage = handleElectionsWebsocketMessage
-    })
+    socket = websocketApi("elections/websocket", websocket_id)
+    socket.onmessage = handleElectionsWebsocketMessage
+
 }
 
 async function hidePopupContainers() {
